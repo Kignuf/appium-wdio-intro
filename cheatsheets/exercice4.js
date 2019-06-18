8da893858b2b6dc6ff1e0c6a35350a3a6da7fd34
Kevin Roulleau <kevin.roulleau@happn.com>
2019-06-18 20:51:41 +0200

    Add solution for exercice 4

---
+++ cheatsheets/exercice4.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+// https://webdriver.io/docs/api/appium.html
+describe('Exercice 4', function() {
+	it('Can deal with webviews', function() {
+		// open webview
+		$('~Webview Demo').click()
+
+		// get context info
+		const currentContext = driver.getContext()
+		console.log('Current context:', currentContext)
+		const availableContexts = driver.getContexts()
+		console.log('Available contexts:', JSON.stringify(availableContexts))
+
+		// type url and submit
+		$('~urlInput').setValue('https://appiumpro.com')
+		$('~navigateBtn').click()
+
+		// save app source (xml)
+		const xml = driver.getPageSource()
+		fs.writeFileSync('./app.xml', xml)
+
+		// set context to webview
+		driver.switchContext(availableContexts.find(context => context.toLowerCase().includes('webview')))
+
+		// save webview source (html)
+		const html = driver.getPageSource()
+		fs.writeFileSync('./website.html', html)
+
+	})
+})
